f7921db0897d8589d3c65b5e56a9dca07c720603
EagleCoder404 <harsha.jediknight@gmail.com>
2021-05-23 00:56:56 +0530

    excel file student dabadebda

--- app/blueprint/main.py
+++ app/blueprint/main.py
@@ -8,3 +8,7 @@ bp = Blueprint("main",__name__,url_prefix="/")
 def index():
     # test = socketio.async_mode
     return render_template('index.html')
+
+@bp.route("/excel_test")
+def excel_test():
+    return render_template("test_excel_parser.html")
