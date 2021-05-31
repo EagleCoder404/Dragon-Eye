8f505f41d8d99a6bdc34dabeef90d24c34d08772
EagleCoder404 <harsha.jediknight@gmail.com>
2021-05-31 18:19:37 +0530

    sd

--- config.py
+++ config.py
@@ -3,8 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class BaseConfiguration(object):
-    SQLALCHEMY_DATABASE_URI = (os.environ.get("DATABASE_URL") and os.environ.get("DATABASE_URL").replace("://", "ql://", 1)) or 'sqlite:///'+os.path.join(basedir,'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = (os.environ.get("DATABASE_URL") and os.environ.get("DATABASE_URL").replace("://", "ql://", 1))
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
