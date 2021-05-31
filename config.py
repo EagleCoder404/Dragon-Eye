ddb3d4b6466a39940503deac366933977e84c605
EagleCoder404 <harsha.jediknight@gmail.com>
2021-05-31 18:21:48 +0530

    sd12

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class BaseConfiguration(object):
     SQLALCHEMY_DATABASE_URI = (os.environ.get("DATABASE_URL") and os.environ.get("DATABASE_URL").replace("://", "ql://", 1))
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
