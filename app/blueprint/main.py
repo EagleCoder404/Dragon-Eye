6ffb8e4a4cde237536bc8d4e1f38762c843d95a3
EagleCoder404 <harsha.jediknight@gmail.com>
2021-07-08 03:30:28 +0530

    lasd

--- app/blueprint/main.py
+++ app/blueprint/main.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request
-
+from app.models import Log
 bp = Blueprint("main",__name__,url_prefix="/")
 
 
@@ -15,3 +15,16 @@ def excel_test():
     print(request.form.get("lol"))
     print()
     return render_template("test.html")
+
+@bp.route("/logs")
+def get_all_logs():
+    logs = Log.query.all()
+    log_csv = []
+    headers = list(logs[0].proctoring_logs[0].keys())
+    for log in logs:
+        for x in log.proctoring_logs:
+            row = [log.id] + list(x.values())
+            row = list(map(str, row))
+            log_csv.append(row)
+    print(log_csv[4:20])
+    return render_template('logs.html',log_csv=log_csv, headers=headers)
